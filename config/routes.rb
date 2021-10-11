f4b40a72b4642e134b3f0f285124343777f6de21
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-10-11 14:06:17 +0000

    [未]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :books
+  resources :books  do
+    resource :favorites, only: [:create, :destroy]
+  end
 
   resources :users, only: [:show,:index,:edit,:update]
-  
+
 end
